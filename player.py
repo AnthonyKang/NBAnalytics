11eb5ac4f85230082cdece050385b03153e06472
Anthony Kang <akang3894@gmail.com>
2015-09-15 21:37:09 -0700

    advnaced stats work

--- player.py
+++ player.py
@@ -2,8 +2,12 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from datetime import datetime
 import re
+from crawler import *
 
-
+totals_labels = ["Season", "Age", "Tm", "Lg", "Pos", "G", "GS", "MP", "FG", "FGA", "FG%", "3P", "3P%", "2P", "2PA", "2P%", "eFG%", "FT", "FTA", "FT%", "ORB", "DRB", "TRB", "AST", "STL", "BLK", "TOV", "PF", "PTS"]
+advanced_labels = totals_labels[0:5] + ["G", "MP", "PER", "TS%", "3PAr", "FTr", "ORB%", "DRB%", "TRB%", "AST%", "STL%", "BLK%", "TOV%", "USG%", "__", "OWS", "DWS", "WS", "WS/48", "__", "OBPM", "DBPM", "BPM", "VORP"]
+print(len(advanced_labels))
+print(len(["PER", "TS%", "3PAr", "FTr", "ORB%", "DRB%", "TRB%", "AST%", "STL%", "BLK%", "TOV%", "USG%", "__", "OWS", "DWS", "WS", "WS/48", "__", "OBPM", "DBPM", "BPM", "VORP"]))
 class Player:
 
 	def __init__(self, url):
@@ -16,33 +20,53 @@ class Player:
 		self.draftYear = self.soup.find_all(string=re.compile("[0-9]+ NBA Draft"))[0][0:4]
 		
 		self.totals_labels = ["Season", "Age", "Tm", "Lg", "Pos", "G", "GS", "MP", "FG", "FGA", "FG%", "3P", "3P%", "2P", "2PA", "2P%", "eFG%", "FT", "FTA", "FT%", "ORB", "DRB", "TRB", "AST", "STL", "BLK", "TOV", "PF", "PTS"]
+		self.poss_labels = self.totals_labels + ["","ORtg", "DRtg"]
+		self.advanced_labels = self.totals_labels[0:5] + ["PER", "TS%", "3PAr", "FTr", "ORB%", "DRB%", "TRB%", "AST%", "STL%", "BLK%", "TOV%", "USG%", "", "OWS", "DWS", "WS", "WS/48", "", "OBPM", "DBPM", "BPM", "VORP"]
 
-
-	def getTotals(self):
+	def getTotals(self, tableName):
+		#tableName = "totals", "per_minute", "per_game", "per_poss", "advanced"
 		totals_table = []
 		for i in range(int(self.draftYear)+1,int(self.curryear)+1):
 			total = {}
 
-			year = self.soup.find(id="totals." + str(i))
+			year = self.soup.find(id=tableName + "." + str(i))
 			table_data = year.find_all('td')
 
-			for j in range(0,len(self.totals_labels)):
+			if tableName == 'per_poss':
+				labels = self.poss_labels
+			elif tableName == 'advanced':
+				labels = advanced_labels
+			else:
+				labels = self.totals_labels
+
+			print(table_data)
+			print(len(labels))
+			for j in range(0,len(labels)):
 				if j == 0 or j == 2 or j == 3:
-					total[self.totals_labels[j]] = table_data[j].find('a').contents[0]
+					total[labels[j]] = table_data[j].find('a').contents[0]
 				else:
-					total[self.totals_labels[j]] = table_data[j].contents[0]
+					if len(table_data[j].contents) > 0:
+						total[labels[j]] = table_data[j].contents[0]
+					#if tableName == 'per_poss':
+					#	print(table_data[j].contents)
+					#	if j != 29:
+					#		total[labels[j]] = table_data[j].contents[0]
+					#else:
+					#	total[labels[j]] = table_data[j].contents[0]
 
 			totals_table.append(total)
 
 		return totals_table
+
+
 		
 
 
 def main():
 	russelWestbrook = Player("http://www.basketball-reference.com/players/w/westbru01.html")
-	russelWestbrook.getTotals()
+	#russelWestbrook.getTotals("per_minute")
 	stephenCurry = Player("http://www.basketball-reference.com/players/c/curryst01.html")
-	print(stephenCurry.getTotals())
+	print(stephenCurry.getTotals("advanced"))
 
 
 
